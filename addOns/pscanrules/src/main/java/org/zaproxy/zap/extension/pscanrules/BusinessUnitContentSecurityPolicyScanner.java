31c156062b03238c6f435ae84c73e5919d9cc32b
Oleksii Kres <oleksii_kres@epam.com>
2020-04-16 00:20:10 +0300

    alert is ready

--- addOns/pscanrules/src/main/java/org/zaproxy/zap/extension/pscanrules/BusinessUnitContentSecurityPolicyScanner.java
+++ addOns/pscanrules/src/main/java/org/zaproxy/zap/extension/pscanrules/BusinessUnitContentSecurityPolicyScanner.java
@@ -16,14 +16,15 @@ import org.parosproxy.paros.network.HttpStatusCode;
 import org.zaproxy.zap.extension.pscan.PassiveScanThread;
 import org.zaproxy.zap.extension.pscan.PluginPassiveScanner;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class BusinessUnitContentSecurityPolicyScanner extends PluginPassiveScanner {
     private static final Logger LOG = Logger.getLogger(PluginPassiveScanner.class);
@@ -39,23 +40,14 @@ public class BusinessUnitContentSecurityPolicyScanner extends PluginPassiveScann
 
     private PassiveScanThread parent = null;
 
-    @Override
-    public void setParent(PassiveScanThread parent) {
-        this.parent = parent;
-    }
 
-    @Override
-    public String getName() {
-        return Constant.messages.getString(MESSAGE_PREFIX + "name");
-    }
-
-    private String getSolution() {
-        return Constant.messages.getString(MESSAGE_PREFIX + "soln");
-    }
-
-    private String getReference() {
-        return Constant.messages.getString(MESSAGE_PREFIX + "refs");
-    }
+    private static final String KV_BU = "kruidvat";
+    private static final String WTC_BU_UA = "watson.ua";
+    private static final String WTC_BU_RU = "watson.ru";
+    private static final String MRN_BU = "marionnaud";
+    private static final String SD_BU = "superdrug";
+    private static final String TPS_BU = "theperfumeshop";
+    private static final String IPXL_BU = "iciparisxl";
 
     @Override
     public void scanHttpResponseReceive(HttpMessage msg, int id, Source source) {
@@ -72,36 +64,67 @@ public class BusinessUnitContentSecurityPolicyScanner extends PluginPassiveScann
         }
 
         List<String> cspHeaderValues = msg.getResponseHeader().getHeaderValues(HTTP_HEADER_CSP);
-
-
-        List<List<String>> result = new ArrayList<>();
+        String cspSample = null;
 
         if (!cspHeaderValues.isEmpty()) {
-            //TODO begin of comparing validation
-            String fileWithCsp = "src/main/resources/00015_security_headers.impex";
-//            String fileWithCsp = "src/main/resources/23_[ES-172]-Security_Config_Parameters.impex";
+            String configFile = "C:\\Projects\\zap_plugin\\zap-extensions\\addOns\\pscanrules\\src\\main\\resources\\config\\bu_csp_configuration";
+
             try {
-                BufferedReader reader = new BufferedReader(new FileReader(fileWithCsp));
+                String buName = URI.parse(msg.getRequestHeader().getURI().toString()).host;
+
+                BufferedReader reader = new BufferedReader(new FileReader(configFile));
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    if (line.contains("headerName")) {
-                        while (!(line = reader.readLine()).contains("#")) {
-
-                            result.add(Arrays
-                                    .stream(line.replace("\"", "")
-                                            .split(";"))
-                                    .skip(5)
-                                    .collect(Collectors.toList()));
-                        }
+                    if (line.contains(buName)) {
+                        cspSample = reader.readLine();
                         break;
                     }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
             String policyText = cspHeaderValues.toString().replaceAll("[\\[\\]]", "");
-            checkIfEquals(result, policyText);
-            List<Notice> notices = new ArrayList<>();
+            //implemented bellow
+            // checkIfEquals(cspSample, policyText);
+
+            List<String> sampleDirectives = Arrays.asList(cspSample.split(";"));
+            List<String> readDirectives = Arrays.asList(policyText.split(";"));
+
+            for (int i = 0; i < sampleDirectives.size(); i++) {
+                if (sampleDirectives.get(i).equals(readDirectives.get(i))) {
+                    System.out.println("BINGO!");
+                } else {
+                    Alert alert =
+                            new Alert(
+                                    getPluginId(),
+                                    Alert.RISK_HIGH,
+                                    Alert.CONFIDENCE_MEDIUM, // PluginID, Risk, Reliability
+                                    "AlertName " + i);
+                    alert.setDetail(
+                            "description", // Description
+                            msg.getRequestHeader().getURI().toString(), // URI
+                            "param", // Param
+                            "attack", // Attack
+                            "other info", // Other info
+                            getSolution(), // Solution
+                            getReference(), // References
+                            "evidence", // Evidence
+                            16, // CWE-16: Configuration
+                            15, // WASC-15: Application Misconfiguration
+                            msg); // HttpMessage
+                    parent.raiseAlert(id, alert);
+                }
+            }
+        }
+    }
+
+    private boolean isNotHtmlResponse(HttpMessage msg) {
+        return (!msg.getResponseHeader().isHtml()
+                || HttpStatusCode.isRedirection(msg.getResponseHeader().getStatusCode()))
+                && !Plugin.AlertThreshold.LOW.equals(this.getAlertThreshold());
+    }
+            /*List<Notice> notices = new ArrayList<>();
             Origin origin = URI.parse(msg.getRequestHeader().getURI().toString());
             Policy policy = ParserWithLocation.parse(policyText, origin, notices);
 
@@ -141,39 +164,57 @@ public class BusinessUnitContentSecurityPolicyScanner extends PluginPassiveScann
             }
 
         } else {
-            /*raiseAlert(
+            *//*raiseAlert(
                     msg,
                     Constant.messages.getString(MESSAGE_PREFIX + "stylesrc.unsafe.name"),
                     id,
                     Constant.messages.getString(MESSAGE_PREFIX + "stylesrc.unsafe.desc"),
                     getHeaderField(msg, HTTP_HEADER_CSP).get(0),
                     Alert.RISK_MEDIUM,
-                    cspHeaderValues.get(0));*/
+                    cspHeaderValues.get(0));*//*
             LOG.debug("CSP do not find!!!");
         }
 
         LOG.debug("\tScan of record "
                 + id
                 + " took "
-                + (LocalDateTime.now().getSecond() - startTime)
+                + (LocalDateTime.now().
+
+                getSecond() - startTime)
                 + " seconds");
 
     }
 
-    private void checkIfEquals(List<List<String>> result, String policyText) {
-        String[] headerCsp = policyText.split(";");
+    private void checkIfEquals(String cspSample, String policyText) {
+        List<String> sampleDirectives = Arrays.asList(cspSample.split(";"));
+        List<String> readDirectives = Arrays.asList(policyText.split(";"));
 
-        for (int i = 0; i < result.size(); i++) {
-            for (int j = 0; j < result.get(i).size(); j++) {
-                if (result.get(i).get(1).equals(headerCsp[i].trim())) {
-                    System.out.println("BINGO");
-                } else {
-                    System.out.println("NOT!");
-                    System.out.println("res = " + result.get(i).get(1));
-                    System.out.println("hed = " + headerCsp[i]);
-                }
+        for (int i = 0; i < sampleDirectives.size(); i++) {
+            if (sampleDirectives.get(i).equals(readDirectives.get(i))) {
+                System.out.println("BINGO!");
+            } else {
+
             }
         }
+
+
+
+        *//*
+        cspSample = Arrays.stream(line.split(";"))
+                .collect(Collectors.toList());*//*
+        String[] headerCsp = policyText.split(";");
+
+   *//*     for (int i = 0; i < result.size(); i++) {
+//            for (int j = 0; j < result.get(i).size(); j++) {
+            if (result.get(i).get(1).equals(headerCsp[i].trim())) {
+                System.out.println("BINGO");
+            } else {
+                System.out.println("NOT!");
+                System.out.println("res = " + result.get(i).get(1));
+                System.out.println("hed = " + headerCsp[i]);
+            }
+//            }
+        }*//*
 
     }
 
@@ -256,58 +297,110 @@ public class BusinessUnitContentSecurityPolicyScanner extends PluginPassiveScann
             }
         }
         return matchedHeaders;
-    }
+    }*/
 
     @Override
     public int getPluginId() {
         return PLUGIN_ID;
     }
 
-    private List<String> getAllowedWildcardSources(String policyText, Origin origin) {
+    /* private List<String> getAllowedWildcardSources(String policyText, Origin origin) {
 
-        List<String> allowedSources = new ArrayList<String>();
-        Policy pol = ParserWithLocation.parse(policyText, origin);
+         List<String> allowedSources = new ArrayList<String>();
+         Policy pol = ParserWithLocation.parse(policyText, origin);
 
-        if (pol.allowsScriptFromSource(PARSED_WILDCARD_URI)) {
-            allowedSources.add("script-src");
-            allowedSources.add("script-src-elem");
-            allowedSources.add("script-src-attr");
-        }
-        if (pol.allowsStyleFromSource(PARSED_WILDCARD_URI)) {
-            allowedSources.add("style-src");
-            allowedSources.add("style-src-elem");
-            allowedSources.add("style-src-attr");
-        }
-        if (pol.allowsImgFromSource(PARSED_WILDCARD_URI)) {
-            allowedSources.add("img-src");
-        }
-        if (pol.allowsConnectTo(PARSED_WILDCARD_URI)) {
-            allowedSources.add("connect-src");
-        }
-        if (pol.allowsFrameFromSource(PARSED_WILDCARD_URI)) {
-            allowedSources.add("frame-src");
-        }
-        if (pol.allowsFrameAncestor(PARSED_WILDCARD_URI)) {
-            allowedSources.add("frame-ancestor");
-        }
-        if (pol.allowsFontFromSource(PARSED_WILDCARD_URI)) {
-            allowedSources.add("font-src");
-        }
-        if (pol.allowsMediaFromSource(PARSED_WILDCARD_URI)) {
-            allowedSources.add("media-src");
-        }
-        if (pol.allowsObjectFromSource(PARSED_WILDCARD_URI)) {
-            allowedSources.add("object-src");
-        }
-        if (pol.allowsManifestFromSource(PARSED_WILDCARD_URI)) {
-            allowedSources.add("manifest-src");
-        }
-        if (pol.allowsWorkerFromSource(PARSED_WILDCARD_URI)) {
-            allowedSources.add("worker-src");
-        }
-        if (pol.allowsPrefetchFromSource(PARSED_WILDCARD_URI)) {
-            allowedSources.add("prefetch-src");
-        }
-        return allowedSources;
+         if (pol.allowsScriptFromSource(PARSED_WILDCARD_URI)) {
+             allowedSources.add("script-src");
+             allowedSources.add("script-src-elem");
+             allowedSources.add("script-src-attr");
+         }
+         if (pol.allowsStyleFromSource(PARSED_WILDCARD_URI)) {
+             allowedSources.add("style-src");
+             allowedSources.add("style-src-elem");
+             allowedSources.add("style-src-attr");
+         }
+         if (pol.allowsImgFromSource(PARSED_WILDCARD_URI)) {
+             allowedSources.add("img-src");
+         }
+         if (pol.allowsConnectTo(PARSED_WILDCARD_URI)) {
+             allowedSources.add("connect-src");
+         }
+         if (pol.allowsFrameFromSource(PARSED_WILDCARD_URI)) {
+             allowedSources.add("frame-src");
+         }
+         if (pol.allowsFrameAncestor(PARSED_WILDCARD_URI)) {
+             allowedSources.add("frame-ancestor");
+         }
+         if (pol.allowsFontFromSource(PARSED_WILDCARD_URI)) {
+             allowedSources.add("font-src");
+         }
+         if (pol.allowsMediaFromSource(PARSED_WILDCARD_URI)) {
+             allowedSources.add("media-src");
+         }
+         if (pol.allowsObjectFromSource(PARSED_WILDCARD_URI)) {
+             allowedSources.add("object-src");
+         }
+         if (pol.allowsManifestFromSource(PARSED_WILDCARD_URI)) {
+             allowedSources.add("manifest-src");
+         }
+         if (pol.allowsWorkerFromSource(PARSED_WILDCARD_URI)) {
+             allowedSources.add("worker-src");
+         }
+         if (pol.allowsPrefetchFromSource(PARSED_WILDCARD_URI)) {
+             allowedSources.add("prefetch-src");
+         }
+         return allowedSources;
+     }
+ */
+    @Override
+    public void setParent(PassiveScanThread parent) {
+        this.parent = parent;
     }
+
+    @Override
+    public String getName() {
+        return Constant.messages.getString(MESSAGE_PREFIX + "name");
+    }
+
+    private String getSolution() {
+        return Constant.messages.getString(MESSAGE_PREFIX + "soln");
+    }
+
+    private String getReference() {
+        return Constant.messages.getString(MESSAGE_PREFIX + "refs");
+    }
+
+
 }
+/*
+enum BusinessUnit {
+    KV_BU(Set.of("kruidvat", "KV")),
+    WTC_BU_UA(Set.of("watsons.ua", "WTC_UA")),
+    WTC_BU_RU(Set.of("watsons.ru", "WTC_RU")),
+    MRN_BU(Set.of("marionnaud", "MRN")),
+    SD_BU(Set.of("superdrug", "SD")),
+    TPS_BU(Set.of("theperfumeshop", "TPS")),
+    IPXL_BU(Set.of("iciparisxl", "IPXL"));
+
+    final Set<String> bu;
+
+    BusinessUnit(Set<String> bu) {
+        this.bu = bu;
+    }
+
+    public String getBUName() {
+        return bu.
+    }
+
+}
+
+
+enum BU {
+    KV_BU,
+    WTC_BU_UA,
+    WTC_BU_RU,
+    MRN_BU,
+    SD_BU,
+    TPS_BU,
+    IPXL_BU;
+}*/
