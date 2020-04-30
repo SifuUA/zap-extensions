c6dc506d1ecd34d0e2d452d5a3ef36238f46c5cb
Oleksii Kres <oleksii_kres@epam.com>
2020-04-30 21:15:29 +0300

    fix founded bug

--- addOns/pscanrules/src/main/java/org/zaproxy/zap/extension/pscanrules/BusinessUnitContentSecurityPolicyScanner.java
+++ addOns/pscanrules/src/main/java/org/zaproxy/zap/extension/pscanrules/BusinessUnitContentSecurityPolicyScanner.java
@@ -50,32 +50,59 @@ public class BusinessUnitContentSecurityPolicyScanner extends PluginPassiveScann
         }
     }
 
-    private void compareCSP(HttpMessage msg, int id, List<String> sampleDirectives, List<String> readDirectives, List<String> cspHeaderValues) {
+    private void compareCSP(HttpMessage msg, int id, List<String> sampleDirectives, List<String> siteDirectives, List<String> cspHeaderValues) {
         for (int i = 0; i < sampleDirectives.size(); i++) {
-            if (sampleDirectives.get(i).equals(readDirectives.get(i))) {
+            if (sampleDirectives.get(i).equals(siteDirectives.get(i))) {
                 LOG.info(getName() + getDirectiveName(sampleDirectives, i) + " is matches the pattern");
             } else {
-                raiseAlert(msg, id, sampleDirectives, readDirectives, i, cspHeaderValues);
+                AlertDto alertDto = new AlertDto.Builder()
+                        .withMsg(msg)
+                        .withId(id)
+                        .withName(getName() + getDirectiveName(sampleDirectives, i))
+                        .withDescription(getDesc() + getAlertName(sampleDirectives, i))
+                        .withEvidence(cspHeaderValues.get(0))
+                        .withDifference(getDifferencesDetails(sampleDirectives, siteDirectives, i))
+                        .build();
+                raiseAlert(alertDto);
             }
+        }
+        if (sampleDirectives.size() > siteDirectives.size()) {
+            raiseNewDirectiveAlert(msg, id, siteDirectives, sampleDirectives, cspHeaderValues);
+        } else if (siteDirectives.size() > sampleDirectives.size()) {
+            raiseNewDirectiveAlert(msg, id, sampleDirectives, siteDirectives, cspHeaderValues);
         }
     }
 
-    private void raiseAlert(HttpMessage msg, int id, List<String> sampleDirectives, List<String> readDirectives, int i, List<String> cspHeaderValues) {
-        Alert alert = new Alert(getPluginId(), Alert.RISK_HIGH, Alert.CONFIDENCE_MEDIUM, // PluginID, Risk, Reliability
-                getName() + getDirectiveName(sampleDirectives, i));
+    private void raiseNewDirectiveAlert(HttpMessage msg, int id, List<String> sampleDirectives, List<String> siteDirectives, List<String> cspHeaderValues) {
+        int difference = siteDirectives.size() - sampleDirectives.size();
+        for (int i = 0; i < difference; i++) {
+            AlertDto alertDto = new AlertDto.Builder()
+                    .withMsg(msg)
+                    .withId(id)
+                    .withName(getName() + getDirectiveName(siteDirectives, siteDirectives.size() - i - 1))
+                    .withDescription(getDesc() + getAlertName(siteDirectives, siteDirectives.size() - i - 1))
+                    .withEvidence(cspHeaderValues.get(0))
+                    .withDifference("Detected new CSP directive - " + siteDirectives.get(siteDirectives.size() - i - 1))
+                    .build();
+            raiseAlert(alertDto);
+        }
+    }
+
+    private void raiseAlert(AlertDto alertDto) {
+        Alert alert = new Alert(getPluginId(), Alert.RISK_HIGH, Alert.CONFIDENCE_MEDIUM, alertDto.getAlertName());
         alert.setDetail(
-                getDesc() + getAlertName(sampleDirectives, i),  // Description
-                msg.getRequestHeader().getURI().toString(), // URI
+                alertDto.getAlertDescription(),  // Description
+                alertDto.getMsg().getRequestHeader().getURI().toString(), // URI
                 getName(), // Param
                 "", // Attack
-                getDifferencesDetails(sampleDirectives, readDirectives, i), // Other info
+                alertDto.getDifference(), // Other info
                 getSolution(), // Solution
                 getReference(), // References
-                cspHeaderValues.get(0), // Evidence
+                alertDto.getEvidence(), // Evidence
                 16, // CWE-16: Configuration
                 15, // WASC-15: Application Misconfiguration
-                msg); // HttpMessage
-        parent.raiseAlert(id, alert);
+                alertDto.getMsg()); // HttpMessage
+        parent.raiseAlert(alertDto.getMsgId(), alert);
         if (LOG.isDebugEnabled()) {
             LOG.info("Alert raised with information" + alert.toString());
         }
@@ -113,9 +140,9 @@ public class BusinessUnitContentSecurityPolicyScanner extends PluginPassiveScann
 
         for (DiffMatchPatch.Diff node : listOfDiff) {
             if (node.operation.name().equals("EQUAL")) {
-                sb.append("This is part is equal: ").append(node.text).append("\n");
+                sb.append("This is part is equal in bu_csp_configuration file and CSP from site: ").append(node.text).append("\n");
             } else if (node.operation.name().equals("INSERT")) {
-                sb.append("This is part was defined in configuration file: ").append(node.text).append("\n");
+                sb.append("This is new CSP directives : ").append(node.text).append("\n");
             } else {
                 sb.append("This is a new directive or some of directive has been modified: ").append(node.text).append("\n");
             }
@@ -163,5 +190,80 @@ public class BusinessUnitContentSecurityPolicyScanner extends PluginPassiveScann
         //File configFile = new File("C:\\Users\\Oleksii_Kres\\Idea Project\\zap_project\\zap-extensions\\addOns\\pscanrules\\src\\main\\zapHomeFiles\\bu_config\\bu_csp_configuration");
         File configFile = new File(Constant.getZapHome() + File.separator + "bu_config" + File.separator + "bu_csp_configuration");
         return configFile;
+    }
+}
+
+class AlertDto {
+    private HttpMessage msg;
+    private int msgId;
+    private String alertName;
+    private String alertDescription;
+    private String evidence;
+    private String difference;
+
+    public HttpMessage getMsg() {
+        return msg;
+    }
+
+    public int getMsgId() {
+        return msgId;
+    }
+
+    public String getAlertName() {
+        return alertName;
+    }
+
+    public String getAlertDescription() {
+        return alertDescription;
+    }
+
+    public String getEvidence() {
+        return evidence;
+    }
+
+    public String getDifference() {
+        return difference;
+    }
+
+    public static class Builder {
+        private AlertDto alertDto;
+
+        public Builder() {
+            alertDto = new AlertDto();
+        }
+
+        public Builder withMsg(HttpMessage msg) {
+            alertDto.msg = msg;
+            return this;
+        }
+
+        public Builder withId(int id) {
+            alertDto.msgId = id;
+            return this;
+        }
+
+        public Builder withName(String name) {
+            alertDto.alertName = name;
+            return this;
+        }
+
+        public Builder withDescription(String description) {
+            alertDto.alertDescription = description;
+            return this;
+        }
+
+        public Builder withEvidence(String evidence) {
+            alertDto.evidence = evidence;
+            return this;
+        }
+
+        public Builder withDifference(String difference) {
+            alertDto.difference = difference;
+            return this;
+        }
+
+        public AlertDto build() {
+            return  alertDto;
+        }
     }
 }
